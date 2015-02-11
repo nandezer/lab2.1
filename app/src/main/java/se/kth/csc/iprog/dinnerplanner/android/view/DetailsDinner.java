d5db8b94a9792f2f39f36fd3941114f71c91370e
nandezer <marchf25@gmail.com>
2015-02-11 14:39:51 +0100

    Update DetailsDinner.java
    (last version)

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/DetailsDinner.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/view/DetailsDinner.java
@@ -1,7 +1,17 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import se.kth.csc.iprog.dinnerplanner.android.R;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 
 /**
@@ -12,9 +22,12 @@ public class DetailsDinner {
     View view;
     DinnerModel model;
 
-    //@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+
     public DetailsDinner(View view, DinnerModel model) {
         this.view = view;
         this.model = model;
+
+        TextView participants = (TextView) view.findViewById(R.id.participants);
+        participants.setText("Guests");
     }
 }
