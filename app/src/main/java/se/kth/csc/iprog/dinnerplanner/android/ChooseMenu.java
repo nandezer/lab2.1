3eca24d0e150275a35ef72ad8b6c68d41b15befe
nandezer <marchf25@gmail.com>
2015-02-11 14:41:03 +0100

    Update ChooseMenu.java

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/ChooseMenu.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/ChooseMenu.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Vector;
@@ -14,6 +16,7 @@ import java.util.Vector;
 import se.kth.csc.iprog.dinnerplanner.android.view.Banner;
 import se.kth.csc.iprog.dinnerplanner.android.view.ButtonStart_Create;
 import se.kth.csc.iprog.dinnerplanner.android.view.DetailsDinner;
+import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 import se.kth.csc.iprog.dinnerplanner.model.Dish;
 import se.kth.csc.iprog.dinnerplanner.android.view.DishDisplay;
@@ -33,9 +36,13 @@ public class ChooseMenu extends Activity {
 
         DinnerModel model = new DinnerModel();
         this.model = model;
-        DishDisplay dish =  new DishDisplay(findViewById(R.id.this_is_dish_view_id), model);
+        Banner bannerView = new Banner(findViewById(R.id.this_is_banner_view_id));
+        ExampleView starters = new ExampleView(findViewById(R.id.starters),"Starters");
+        ExampleView mainCourses = new ExampleView(findViewById(R.id.mainCourses),"Main Courses");
+        ExampleView desserts = new ExampleView(findViewById(R.id.desserts),"Desserts");
         DetailsDinner details =  new DetailsDinner(findViewById(R.id.this_is_details_dinner_view_id), model);
         ButtonStart_Create start = new ButtonStart_Create(findViewById(R.id.this_is_buttons_start_create_view_id), "Create");
+
 
 
     }
@@ -45,13 +52,13 @@ public class ChooseMenu extends Activity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_start:
-                Intent intent = new Intent();
-                Bundle b = new Bundle();
-                b.putParcelable("Model", (Parcelable)model);
-                intent.putExtras(b);
-                intent.setClass(this, InstructionsMenu.class);
-                startActivity(intent);
+                DinnerModel modelSend = this.model;
+                Intent i = new Intent(this, InstructionsMenu.class);
+                //i.putExtras("DinnerModel", modelSend);
+                startActivity(i);
                 break;
+            case R.id.banner:
+                this.model = new DinnerModel();
         }
     }
 
