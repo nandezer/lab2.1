6b3e757ba174e993bc967b8199d7aaa7cdd06827
nandezer <marchf25@gmail.com>
2015-02-11 14:41:28 +0100

    Update InstructionsMenu.java

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/InstructionsMenu.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/InstructionsMenu.java
@@ -3,12 +3,16 @@ package se.kth.csc.iprog.dinnerplanner.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
-
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import se.kth.csc.iprog.dinnerplanner.android.view.Banner;
 import se.kth.csc.iprog.dinnerplanner.android.view.ButtonStart_Create;
-import se.kth.csc.iprog.dinnerplanner.android.view.DetailsDinner;
-import se.kth.csc.iprog.dinnerplanner.android.view.DishDisplay;
-import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
 
 
 public class InstructionsMenu extends Activity {
@@ -20,25 +24,22 @@ public class InstructionsMenu extends Activity {
 
         // Set the view for the main activity screen
         // it must come before any call to findViewById method
-        setContentView(R.layout.choose_menu);
-
-        DinnerModel model = new DinnerModel();
-        DishDisplay dish =  new DishDisplay(findViewById(R.id.this_is_dish_view_id), model);
-        DetailsDinner details =  new DetailsDinner(findViewById(R.id.this_is_details_dinner_view_id), model);
-        
+        setContentView(R.layout.instructions_menu);
+        Banner bannerView = new Banner(findViewById(R.id.banner_instructions));
+        ExampleView ingredients = new ExampleView(findViewById(R.id.selectedIngredients),"Ingredients");
 
 
     }
 
 
 
-    /*public void onClick(View view) {
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_start:
-                startActivity(new Intent(this, .class));
+                startActivity(new Intent(this, MainActivity.class));
                 break;
         }
-    }*/
+    }
 
 
 }
