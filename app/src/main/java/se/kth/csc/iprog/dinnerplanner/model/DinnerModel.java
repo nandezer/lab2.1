4a799b5514e8bb0072a6993ba31f691491c9768e
Isaac Rondón <isaac.jr.sosa@gmail.com>
2015-02-20 09:53:36 +0100

    Adding Observer pattern and adjusting XML for Instruction menu screen

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/model/DinnerModel.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/model/DinnerModel.java
@@ -3,8 +3,9 @@ package se.kth.csc.iprog.dinnerplanner.model;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Observable;
 
-public class DinnerModel implements Serializable {
+public class DinnerModel extends Observable implements Serializable {
 
 
     Set<Dish> dishes = new HashSet<Dish>();
@@ -55,6 +56,32 @@ public class DinnerModel implements Serializable {
         dish2.addIngredient(dish2ing11);
         dishes.add(dish2);
 
+        Dish dish3 = new Dish("Starter2",Dish.STARTER,"toast","Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.");
+        Ingredient dish3ing1 = new Ingredient("extra lean ground beef",115,"g",20);
+        Ingredient dish3ing2 = new Ingredient("sea salt",0.7,"g",3);
+        Ingredient dish3ing3 = new Ingredient("small onion, diced",0.25,"",2);
+        Ingredient dish3ing4 = new Ingredient("garlic salt",0.6,"g",3);
+        Ingredient dish3ing5 = new Ingredient("Italian seasoning",0.3,"g",3);
+        Ingredient dish3ing6 = new Ingredient("dried oregano",0.3,"g",3);
+        Ingredient dish3ing7 = new Ingredient("crushed red pepper flakes",0.6,"g",3);
+        Ingredient dish3ing8 = new Ingredient("Worcestershire sauce",16,"ml",7);
+        Ingredient dish3ing9 = new Ingredient("milk",20,"ml",4);
+        Ingredient dish3ing10 = new Ingredient("grated Parmesan cheese",5,"g",8);
+        Ingredient dish3ing11 = new Ingredient("seasoned bread crumbs",115,"g",4);
+        dish2.addIngredient(dish3ing1);
+        dish2.addIngredient(dish3ing2);
+        dish2.addIngredient(dish3ing3);
+        dish2.addIngredient(dish3ing4);
+        dish2.addIngredient(dish3ing5);
+        dish2.addIngredient(dish3ing6);
+        dish2.addIngredient(dish3ing7);
+        dish2.addIngredient(dish3ing8);
+        dish2.addIngredient(dish3ing9);
+        dish2.addIngredient(dish3ing10);
+        dish2.addIngredient(dish3ing11);
+        dishes.add(dish3);
+
+
     }
 
     /**
@@ -99,8 +126,11 @@ public class DinnerModel implements Serializable {
     public int getNumberOfGuests(){
         return guestNum;
     }
+
     public void setNumberOfGuests(int numberOfGuests){
         guestNum = numberOfGuests;
+        setChanged();
+        notifyObservers();
     }
 
     public int getNumberOfDishes(){
@@ -110,6 +140,8 @@ public class DinnerModel implements Serializable {
     }
     public void setNumberOfDishes(int numberOfDishes){
         dishNum = numberOfDishes;
+        setChanged();
+        notifyObservers();
     }
 
     /**
@@ -163,6 +195,8 @@ public class DinnerModel implements Serializable {
      */
     public void addDishToMenu(Dish dish){
         selectedDishes.add(dish);
+        setChanged();
+        notifyObservers();
     }
 
     /**
@@ -170,6 +204,8 @@ public class DinnerModel implements Serializable {
      */
     public void removeDishFromMenu(Dish dish){
         selectedDishes.remove(dish);
+        setChanged();
+        notifyObservers();
     }
 
 }
